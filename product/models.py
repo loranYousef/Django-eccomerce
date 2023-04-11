ab8631ce6074ca201d212c10b74cb479fc7fd027
loranYousef <loganyousef@gmail.com>
2023-04-11 22:55:33 +0200

    add template

--- product/models.py
+++ product/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from taggit.managers import TaggableManager
 from django.utils.translation import gettext_lazy as _
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -21,6 +23,7 @@ class Product(models.Model):
     tags = TaggableManager()
     subtitle = models.TextField(_('subtitle'),max_length=500)
     description = models.TextField(_('description'),max_length=2800)
+    slug= models.SlugField(null=True,blank=True)
 
     def __str__(self):
         return self.name
@@ -33,30 +36,35 @@ class Product(models.Model):
 
 
 class ProductImages(models.Model):
-    pass
-    '''
-    product:foreignkey
-    image
+    product =models.ForeignKey(Product,verbose_name=_('product'),related_name='product_image',on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='product_images/')
+    
+    
+    def __str__(self):
+        return str(self.product)
+    
 
-    '''
-
+ 
 
 
 class Brand(models.Model):
-    pass
-    '''
-    name
-    image
+    name = models.CharField(_('brand'),max_length=50)
+    image = models.ImageField(_('image'),upload_to='brand/')
+
+    def __str__(self):
+        return self.name
     
-    '''
 
 
 class Reviews(models.Model):
-    pass
-    '''
-    user
-    product
-    review
-    date
-    rate
-    '''
+    user=models.ForeignKey(User, verbose_name=_('user'),related_name='review_author', on_delete=models.SET_NULL,null=True,blank=True)
+    product =models.ForeignKey(Product,verbose_name=_('product'),related_name='product_review',on_delete=models.CASCADE)
+    comment= models.CharField(_('comment'),max_length=200)
+    rate = models.IntegerField(_('rate'))
+    created_at= models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return str(self.product)
+    
+
+
