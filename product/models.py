27a6dc726963e07c4535e284e46da8709844d87f
loranYousef <loganyousef@gmail.com>
2023-04-11 22:12:50 +0200

    design product model

--- product/models.py
+++ product/models.py
@@ -1,3 +1,62 @@
 from django.db import models
+from taggit.managers import TaggableManager
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
+
+PRODUCT_FLAG =(
+    ('Sale','Sales'),
+    ('Feature','Feature'),
+    ('New','New'),
+
+)
+class Product(models.Model):
+
+    name = models.CharField(_('name'),max_length=150)
+    image = models.ImageField(_('image'),upload_to='products/', default='default.png')
+    flag = models.CharField(_('flag'),max_length=10,choices=PRODUCT_FLAG)
+    price = models.FloatField(_('price'))
+    sku = models.IntegerField(_('sku'))
+    brand = models.ForeignKey('Brand',verbose_name=('brand'),related_name='product_brand',on_delete=models.CASCADE)
+    tags = TaggableManager()
+    subtitle = models.TextField(_('subtitle'),max_length=500)
+    description = models.TextField(_('description'),max_length=2800)
+
+    def __str__(self):
+        return self.name
+    
+     
+
+
+
+
+
+
+class ProductImages(models.Model):
+    pass
+    '''
+    product:foreignkey
+    image
+
+    '''
+
+
+
+class Brand(models.Model):
+    pass
+    '''
+    name
+    image
+    
+    '''
+
+
+class Reviews(models.Model):
+    pass
+    '''
+    user
+    product
+    review
+    date
+    rate
+    '''
--- src/settings.py
+++ src/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'product',
-    
+
 ]
 
 MIDDLEWARE = [
@@ -118,6 +118,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    
+    
+]
+MEDIA_URL= 'media/'
+MEDIA_ROOT= BASE_DIR / "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
