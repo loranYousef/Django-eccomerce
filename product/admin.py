981c244dc8f742ea62f90204d595e3858882bf9e
loranYousef <loganyousef@gmail.com>
2023-04-19 10:23:28 +0200

    add product models , admin

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Product , ProductImages , Brand , Reviews
+
+admin.site.register(Product)
+admin.site.register(ProductImages)
+admin.site.register(Brand)
+admin.site.register(Reviews)
