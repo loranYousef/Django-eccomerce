3652019981bf0154b20263374d6010141f61f1b4
loranYousef <loganyousef@gmail.com>
2023-04-20 10:37:43 +0200

    add base template, static ,productlist template

--- src/settings.py
+++ src/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'src.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['src/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
