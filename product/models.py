1e72e93e38e80cc1f5429385387b2c291fb980a2
loranYousef <loganyousef@gmail.com>
2023-04-20 11:26:22 +0200

    work on project

--- product/models.py
+++ product/models.py
@@ -3,6 +3,8 @@ from taggit.managers import TaggableManager
 from django.utils.translation import gettext_lazy as _
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.utils.text import slugify
+
 
 # Create your models here.
 
@@ -27,6 +29,8 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+    
+    
     
      
 
