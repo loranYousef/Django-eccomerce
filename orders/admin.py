67eae65e32ec57a2be221fad549c1f194a08ee41
loranYousef <loganyousef@gmail.com>
2023-04-24 23:10:44 +0200

    design orders models

--- orders/admin.py
+++ orders/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Order , OrderDetail
+from .models import Order , OrderDetail, Cart , CartDetail
 
 admin.site.register(Order)
 admin.site.register(OrderDetail)
+
+admin.site.register(Cart)
+admin.site.register(CartDetail)
