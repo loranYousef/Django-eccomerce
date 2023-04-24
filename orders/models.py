87f6eca642e7f930172d3f2021bd81d0303900b5
loranYousef <loganyousef@gmail.com>
2023-04-24 22:31:56 +0200

    design orders and models

--- orders/models.py
+++ orders/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from product.models import Product
+from django.contrib.auth.models import User
 
 
 ORDER_STATUS = (
@@ -17,16 +18,30 @@ ORDER_STATUS = (
 
 class Order(models.Model):
     order_code= models.CharField(max_length=10)
-    user= ''
+    user= models.ForeignKey(User,related_name='user_order',on_delete=models.SET_NULL,null=True,blank=True)
     order_status = models.CharField(max_length=10, choices= ORDER_STATUS, default='Recieved')
-    delivery_date = models.DateTimeField()
+    delivery_date = models.DateTimeField(null=True,blank=True)
     order_date = models.DateTimeField(default= timezone.now)
 
+    def __str__(self):
+        return self.order_code
+    
 
 
-class OrderDetails(models.Model):
+
+class OrderDetail(models.Model):
     order=models.ForeignKey(Order,related_name='order_detail',on_delete=models.CASCADE)
     product= models.ForeignKey(Product,related_name='order_product',on_delete=models.SET_NULL,null=True, blank=True)
+    price =models.FloatField()
+    quantity = models.IntegerField(default=1)
+    total = models.FloatField(null=True,blank=True)
+
+    def __str__(self):
+        return str(self.order)
+    
+    def save(self, *args, **kwargs):
+       self.total =self.price * self.quantity
+       super(OrderDetail, self).save(*args, **kwargs) 
     
 
 
