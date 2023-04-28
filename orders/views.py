3f6a5c4cea901cf04deec12dc75aaec6edaa339e
loranYousef <loganyousef@gmail.com>
2023-04-28 10:54:08 +0200

    add classbasedviews

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Order
 
-# Create your views here.
+
+class OrderList(ListView):
+    model = Order
