b93d55929d6e5daba9d84212b5c3349a8fc9260f
loranYousef <loganyousef@gmail.com>
2023-11-06 10:29:10 +0100

    fix brand detail paginations

--- product/views.py
+++ product/views.py
@@ -1,3 +1,4 @@
+from typing import Any
 from django.shortcuts import render
 from django.views.generic import ListView , DetailView
 from django.db.models import Count
@@ -22,10 +23,25 @@ class BrandList(ListView):
     queryset = Brand.objects.all().annotate(product_count= Count('product_brand'))
 
 
-class Brand_Detail(DetailView):
-    model = Brand
+class Brand_Detail(ListView):
+    model = Product
+    paginate_by = 50
+    template_name = 'product/brand_detail.html'
+
     # queryset = Brand.objects.filter(slug=slug).annotate(product_count= Count('product_brand'))
     def get_queryset(self):
-        queryset = Brand.objects.filter(slug=self.kwargs['slug']).annotate(product_count= Count('product_brand'))
+        brand = Brand.objects.get(slug=self.kwargs['slug'])
+        queryset = Product.objects.filter(brand=brand)
         return queryset
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        # context["brand"] = Brand.objects.filter(slug=self.kwargs['slug']).annotate(product_count= Count('product_brand'))
+
+        data = Brand.objects.filter(slug=self.kwargs['slug']).annotate(product_count= Count('product_brand'))[0]
+        print(f"Brand : {data.name}")
+        print(f"Brand : {data.image}")
+        context["brand"] = data
+        return context
     
