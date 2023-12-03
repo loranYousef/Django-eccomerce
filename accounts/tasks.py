644a7b040c41de8ffed84b74d741cc69a3d400cc
loranYousef <loganyousef@gmail.com>
2023-12-03 12:16:16 +0100

    add celery to system

--- accounts/tasks.py
+++ accounts/tasks.py
@@ -1,9 +1,11 @@
 from celery import shared_task
 import time 
 
+
+
 @shared_task
 def send_build_emails(n):
-    time.sleep(10)
+    time.sleep(1)
     for x in range(n):
         print(f"Sending email to {x}")
 
